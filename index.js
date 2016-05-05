a72920188f536f7caed5561d2c3e571bfbd2c013
Justin Huff <jjhuff@mspin.net>
2016-05-05 11:29:55 -0700

    wrap with react-prefixer

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
-
-const isStyle = node => node.name.name === 'style';
-
 export default function ({ types: t }) {
-
-  function wrapStyle(path) {
-    //path.replaceWith(path);
-    console.log(path);
-  }
-
   return {
     visitor: {
       JSXAttribute(path) {
-        if (isStyle(path.node)) {
-          wrapStyle(path.get('value.expression'));
+        let { node } = path;
+        // Only work on style attributes
+        if (!t.isJSXIdentifier(node.name, { name: "style" })) {
+          return;
         }
+
+        // Prevent from visiting multiple times
+        if (path.getData('_stylistVisited')) return;
+        path.setData('_stylistVisited', true);
+
+        // Process the value
+        let value = node.value;
+        if (t.isStringLiteral(value)) {
+            // Strings don't get wrapped
+            return;
+        } else if (t.isJSXExpressionContainer(value)) {
+            // Unwrap JSXExpressionContainer
+            value = value.expression;
+        }
+
+        const wrapper = this.addImport("react-prefixer", "default");
+
+        path.replaceWith(
+          t.JSXAttribute(
+            node.name,
+            t.JSXExpressionContainer(
+              t.callExpression(wrapper, [ value ])
+            )
+          )
+        );
       }
     }
   };
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,11 +1,54 @@
 import getCode from './get-code';
 import test from 'tape';
 
-const CODE_STRING = '<div style="anything" />';
+const CODE_STRING = '<div style="anything" foo="bar"/>';
 test(CODE_STRING, t => {
   const code = getCode(CODE_STRING);
-  console.log(code);
+  // Shouldn't be wrapped
   t.ok(code.indexOf('style: "anything"') !== -1);
   t.end();
 });
 
+const CODE = [
+  // inline style
+  '<div style={{alignItems: "center", display: "flex", width: 100}} />',
+
+  // variable outside
+  `const style={alignItems: "center", display: "flex", width: 100};
+  <div style={style} />`,
+
+  // variable outside, key in object
+  `const style={stuff: {alignItems: "center", display: "flex", width: 100}};
+  <div style={style.stuff} />`,
+
+  // object shorthand
+  `const display = "flex";
+  const style={alignItems: "center", display, width: 100};
+  <div style={style} />`,
+
+  // object spread
+  `const style={alignItems: "center", display: "flex"};
+  const finalStyle={...style, width: 100};
+  <div style={finalStyle} />`,
+
+  // array
+  `const styleA={alignItems: "center", display: "flex"};
+  const styleB={width: 100};
+  <div style={[styleA, styleB]} />`,
+
+  // _default in use
+  `const _default=1;
+  <div style={{alignItems: "center", display: "flex", width: 100}} />`,
+
+];
+
+CODE.forEach(src => {
+  test(src, t => {
+    const code = getCode(src);
+    console.log(code);
+    console.log("------------");
+    t.ok(code.match(/import [^\s]* from "react-prefixer";/), "import");
+    t.ok(code.match(/style: [^\(]*\(/), "wrapper");
+    t.end();
+  })
+});
